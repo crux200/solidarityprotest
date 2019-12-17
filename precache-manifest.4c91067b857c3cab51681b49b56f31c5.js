180304823c4f3f9629dc5cbeb1163622a5c949b1
Varghese Thomas <varghesedev@outlook.com>
2019-12-16 20:41:19 -0800

    Updates

---
+++ precache-manifest.4c91067b857c3cab51681b49b56f31c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1c67b66097114b9ac07c5a636460d3",
+    "url": "/solidarityprotest/index.html"
+  },
+  {
+    "revision": "4366a8a977588c0d640a",
+    "url": "/solidarityprotest/static/css/main.8a88cb8d.chunk.css"
+  },
+  {
+    "revision": "4584bd7d566b2e4beea5",
+    "url": "/solidarityprotest/static/js/2.f905d08a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/solidarityprotest/static/js/2.f905d08a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4366a8a977588c0d640a",
+    "url": "/solidarityprotest/static/js/main.b8ac650a.chunk.js"
+  },
+  {
+    "revision": "228f845de9ac7a1f3018",
+    "url": "/solidarityprotest/static/js/runtime-main.b82d590b.js"
+  }
+]);
